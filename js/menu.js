91a3c6d1b4cb2b891c8b9189ab1e6dd836c7ac4f
Eduardo <eduardocandaten10@gmail.com>
2024-06-10 00:03:38 -0300

    acessibilidades do menu

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,25 @@
 const botaoMenu = document.getElementById("botao-menu");
-const headerMenu = document.querySelector(".header ul");
 
-function abrirMenu() {
+function abrirMenu(event) {
+  // previna a o touch de abrir o menu, apenas o click
+  if ((event.type = "touchstart")) event.preventDefault();
+
+  // classes para o funcionamento do botão
+  const headerMenu = document.querySelector(".header ul");
   botaoMenu.classList.toggle("ativo");
   headerMenu.classList.toggle("ativo");
+  const ativo = headerMenu.classList.contains("ativo");
+
+  // modifica o valor do aria-expanded
+  event.currentTarget.setAttribute("aria-expanded", ativo);
+
+  // modifica o valor do aria-label
+  if (ativo) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir menu");
+  }
 }
 
 botaoMenu.addEventListener("click", abrirMenu);
+botaoMenu.addEventListener("touchstart", abrirMenu); // para que não tenha demora no uso mobile
